5ff2704421af39023c61ad33a50b67a429e003c2
horlard <oladipuposaulih@gmail.com>
2019-12-06 18:51:56 +0100

    movie search component

---
+++ src/components/searchBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+const SearchBar=()=> {
+    return (
+        <div>
+            <input/>
+            <i className="search icon"></i>
+        </div>
+    )
+}
+
+export default SearchBar;
