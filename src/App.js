6c70b805d0da04680f969b30313a2aafa39fc54e
horlard <oladipuposaulih@gmail.com>
2019-12-28 02:24:09 +0100

    created my routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
 
 
-import Header from './components/header'
+
+
+import Header from './components/header';
+import Home from './components/home';
+import MoviesDetails from './components/moviesDetails';
 
 
 const App =()=> {
   return (
-    <Header/>
+    <React.Fragment>
+      <Router>
+        <Header/>
+        <Route path='/' exact component={Home}/>
+        <Route path='/movie/:id' component={MoviesDetails}/>
+      </Router>
+        
+
+    </React.Fragment>
+    
   )
 }
 
