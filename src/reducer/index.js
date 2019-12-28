6e22d087968399c8132790207291413754125d82
horlard <oladipuposaulih@gmail.com>
2019-12-28 02:29:37 +0100

    overall reducer component

---
+++ src/reducer/index.js
@@ -0,0 +1,10 @@
+import {combineReducers} from 'redux'
+
+import IdSearch from './idSearch';
+import ResultReducer from './resultReducer';
+
+
+export default combineReducers({
+    IdSearch,
+    ResultReducer
+})
