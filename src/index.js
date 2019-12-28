0347f76784497e4e50d8ea26de3c4561a9e6bfd2
horlard <oladipuposaulih@gmail.com>
2019-12-28 02:24:43 +0100

    gave access to redux and react-redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore,applyMiddleware,compose} from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
+import Reducers from './reducer/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store=createStore(Reducers,composeEnhancers(applyMiddleware(thunk)))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+     document.getElementById('root')
+    );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
